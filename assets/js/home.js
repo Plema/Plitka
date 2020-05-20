4a544ded4e1a3fb586a9ed58cfd14fee7ef5e47c
OdrinskayaMargarita <margarita.odrinskaya@gmail.com>
2020-05-20 19:11:17 +0300

    ready to the test

--- assets/js/home.js
+++ assets/js/home.js
@@ -19,5 +19,31 @@ $(document).ready(function(){
       $('.back-dark').stop().removeClass('back-dark-open');  
       $('body').css('overflow','auto');
       $('.nav').stop().toggleClass('nav-none');
+      $('.popUp').stop().removeClass('open-pop');
+    });
+
+    $( ".one-tekstura" ).click(function() {
+      $('.one-tekstura').stop().removeClass('changed');
+      $(this).stop().toggleClass('changed');
+      $('.one-plitka').stop().toggleClass('open-pop');
+      $('.back-dark').stop().toggleClass('back-dark-open'); 
+      $('body').css('overflow','hidden');
+    });
+
+    $( ".call" ).click(function() {
+      $('.request-call').stop().toggleClass('open-pop');
+      $('.back-dark').stop().toggleClass('back-dark-open'); 
+      $('body').css('overflow','hidden');
+    });
+    $( ".open-politic" ).click(function() {
+      $('.politic').stop().toggleClass('open-pop');
+      $('.back-dark').stop().toggleClass('back-dark-open'); 
+      $('body').css('overflow','hidden');
+    });
+
+    $( ".close-pop" ).click(function() {
+      $('.popUp').stop().removeClass('open-pop');
+      $('.back-dark').stop().removeClass('back-dark-open'); 
+      $('body').css('overflow','visible');
     });
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [55.793212, 37.489257],
+          zoom: 16
+      }),
 
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          balloonContent: 'ул. Маршала Соколовского, 3'
+      });
+
+
+  myMap.geoObjects.add(myPlacemark);
 });
-
-console.log('win')
